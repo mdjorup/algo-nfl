341b4394fc0eefe11c166a408d18938bced0df63
mdjorup <michael.djorup@gmail.com>
2024-08-30 22:26:00 -0400

    even better metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,27 +9,50 @@ const fontSans = FontSans({
 })
 
 export const metadata: Metadata = {
-  title: "NFL Probabilities | Game Predictions for NFL Postseason",
-  description: "Get accurate NFL game probabilities and predictions for the postseason. Stay updated with the latest odds and forecasts for upcoming NFL playoff games.",
-  keywords: "NFL, football, probabilities, predictions, postseason, playoffs, odds, forecasts",
+  title: "NFL Playoff Predictions & Probabilities | NFL Game Odds",
+  description: "Get NFL playoff predictions and game probabilities. Our advanced algorithms provide accurate forecasts for every postseason matchup. Stay ahead with expert NFL odds and analysis.",
+  keywords: "NFL playoffs, football predictions, game probabilities, postseason odds, live NFL forecasts, playoff matchups, NFL analytics",
   authors: [
     {
       name: "Michael Djorup",
       url: "https://x.com/michaeldjorup",
     },
     {
+      name: "NFL.com",
       url: "https://www.nfl.com/"
     }
   ],
-  robots: "index, follow",
+  creator: "Michael Djorup",
+  publisher: "NFLProbabilities.com",
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
   openGraph: {
-    title: "NFL Probabilities | Game Predictions for NFL Postseason",
+    title: "NFL Playoff Predictions & Game Probabilities",
     siteName: "NFLProbabilities.com",
     locale: "en_US",
-    description: "Get accurate NFL game probabilities and predictions for the postseason. Stay updated with the latest odds and forecasts for upcoming NFL playoff games.",
+    description: "Access real-time NFL playoff predictions and game probabilities. Our cutting-edge algorithms deliver precise forecasts for every postseason matchup. Get the edge with expert NFL odds and in-depth analysis.",
     url: "https://nflprobabilities.com",
     type: "website",
+    images: [
+    ],
   },
+  twitter: {
+    card: "summary_large_image",
+    creator: "@michaeldjorup",
+    title: "NFL Playoff Predictions & Game Probabilities",
+    description: "Get the latest NFL playoff predictions and game probabilities.",
+    images: [],
+  },
+  category: "Sports Analytics",
 };
 
 export default function RootLayout({
