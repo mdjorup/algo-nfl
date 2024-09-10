16b057592710c95bc1396338ca18ee21b1ed2299
mdjorup <michael.djorup@gmail.com>
2024-09-09 22:01:41 -0400

    formatting game page better

--- src/app/(body)/games/[eventId]/page.tsx
+++ src/app/(body)/games/[eventId]/page.tsx
@@ -1,6 +1,7 @@
 import SportsbookOddsTable from '@/components/SportsbookOddsTable';
-import { getEvent_cached, getEventOdds_cached } from '@/lib/cacheFunctions';
 import { COLORS } from '@/lib/consts';
+import { getEvent, getEventOdds, getWinProbability } from '@/lib/dbFns';
+import { formatAsPercent } from '@/lib/format-utils';
 import { EventOdds } from '@/lib/types';
 import { format } from 'date-fns';
 import Image from 'next/image';
@@ -64,7 +65,7 @@ const averageWindow = (window: EventOdds[]): EventOdds => {
 const GamePage = async ({ params }: { params: { eventId: string } }) => {
 
 
-  const [event, eventOdds] = await Promise.all([getEvent_cached(params.eventId), getEventOdds_cached(params.eventId)]);
+  const [event, eventOdds] = await Promise.all([getEvent(params.eventId), getEventOdds(params.eventId)]);
 
   const eventOddsData = eventOdds.map((odds) => {
     return {
@@ -73,13 +74,23 @@ const GamePage = async ({ params }: { params: { eventId: string } }) => {
     }
   });
 
-  // console.log(eventOdds)
+
+  const commenceTime = new Date(event.commence_time);
+
+
   const averagedOdds = averageOddsDataPoints(eventOddsData);
 
   const latestSportsbookOdds = eventOddsData.sort((a, b) => b.timestamp.getTime() - a.timestamp.getTime()).slice(0, 10);
 
 
   const isGameActive = new Date(event.commence_time) <= new Date() && !event.completed;
+  const chartOdds = eventOddsData.filter((odds) => {
+    if (isGameActive || event.completed) {
+      return odds.timestamp.getTime() >= commenceTime.getTime();
+    } else {
+      return true;
+    }
+  }).sort((a, b) => a.timestamp.getTime() - b.timestamp.getTime());
 
   const currentOdds = latestSportsbookOdds[latestSportsbookOdds.length - 1]
 
@@ -87,18 +98,14 @@ const GamePage = async ({ params }: { params: { eventId: string } }) => {
     return format(new Date(date), 'MMM d, h:mm a');
   };
 
-  const calculateWinProbability = (odds: EventOdds) => {
-    const totalOdds = odds.home_odds + odds.away_odds;
-    return {
-      home: ((1 / odds.home_odds) / totalOdds * 100).toFixed(1),
-      away: ((1 / odds.away_odds) / totalOdds * 100).toFixed(1)
-    };
-  };
 
-  const winProbabilities = currentOdds ? calculateWinProbability(currentOdds) : null;
+
+  const winProbabilities = currentOdds ? getWinProbability(currentOdds.home_odds, currentOdds.away_odds) : null;
 
   const homeColor = COLORS[event.home_name]; // #123abc
   const awayColor = COLORS[event.away_name]; // #abc123
+
+  const centerTimeString = event.completed ? 'Final Score' : new Date() < event.commence_time ? formatGameTime(event.commence_time) : 'In Progress';
   // 
 
   return (
@@ -112,13 +119,13 @@ const GamePage = async ({ params }: { params: { eventId: string } }) => {
           <div>
             <h2 className="text-2xl font-bold">{event.away_name}</h2>
             {isGameActive && winProbabilities && (
-              <p className="text-sm text-gray-600">{winProbabilities.away}% win probability</p>
+              <p className="text-sm text-gray-600">{formatAsPercent(winProbabilities.awayWinProbability)} win probability</p>
             )}
           </div>
         </div>
         <div className="text-center">
           <p className="text-lg font-semibold mb-2">
-            {event.completed ? 'Final Score' : formatGameTime(event.commence_time)}
+            {centerTimeString}
           </p>
           {event.completed ? (
             <p className="text-3xl font-bold">{event.away_score} - {event.home_score}</p>
@@ -135,14 +142,14 @@ const GamePage = async ({ params }: { params: { eventId: string } }) => {
           <div className="text-right">
             <h2 className="text-2xl font-bold">{event.home_name}</h2>
             {isGameActive && winProbabilities && (
-              <p className="text-sm text-gray-600">{winProbabilities.home}% win probability</p>
+              <p className="text-sm text-gray-600">{formatAsPercent(winProbabilities.homeWinProbability)} win probability</p>
             )}
           </div>
           <Image src={`/${event.home_name}.png`} alt={event.home_name} width={64} height={64} />
         </div>
       </div>
+      <ProbabilityChart homeTeam={event.home_name} awayTeam={event.away_name} data={chartOdds} />
       {!event.completed && <SportsbookOddsTable awayTeam={event.away_name} homeTeam={event.home_name} data={latestSportsbookOdds} />}
-      <ProbabilityChart homeTeam={event.home_name} awayTeam={event.away_name} data={averagedOdds} />
     </div>
   )
 }
